247647a52f62e0441ace420ba1bdfe05642129e7
Nasem Baki <nbaki@mail.med.upenn.edu>
2014-02-20 13:14:36 -0500

    Styled containers differently to mimic v3; added colors and extra styling

--- header.php
+++ header.php
@@ -16,46 +16,103 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<script>
-    $(document).ready(function() {
-        setHeaderDefaults();
-        setMobileNavigation();
-    });
-</script>
-<?php if (is_page('Home')) : ?>
-    <script>
-        $(document).ready(function () {
-            setActiveTab('Home');
-        });
-    </script>
-<?php elseif (is_page('About Us')) : ?>
-    <script>
-        $(document).ready(function () {
-            setActiveTab('About Us');
-        });
-    </script>
-<?php elseif (is_page('Services')) : ?>
-    <script>
-        $(document).ready(function () {
-            setActiveTab('Services');
-        });
-    </script>
-<?php elseif (is_page('Our Process')) : ?>
-    <script>
-        $(document).ready(function () {
-            setActiveTab('Our Process');
-        });
-    </script>
-<?php elseif (is_page('Contact Us')) : ?>
-    <script>
-        $(document).ready(function () {
-            setActiveTab('Contact Us');
-        });
-    </script>
-<?php endif; ?>
+<!--<script>-->
+<!--    $(document).ready(function() {-->
+<!--        setHeaderDefaults();-->
+<!--        setMobileNavigation();-->
+<!--    });-->
+<!--</script>-->
+<?php //if (is_page('Home')) : ?>
+<!--    <script>-->
+<!--        $(document).ready(function () {-->
+<!--            setActiveTab('Home');-->
+<!--        });-->
+<!--    </script>-->
+<?php //elseif (is_page('About Us')) : ?>
+<!--    <script>-->
+<!--        $(document).ready(function () {-->
+<!--            setActiveTab('About Us');-->
+<!--        });-->
+<!--    </script>-->
+<?php //elseif (is_page('Services')) : ?>
+<!--    <script>-->
+<!--        $(document).ready(function () {-->
+<!--            setActiveTab('Services');-->
+<!--        });-->
+<!--    </script>-->
+<?php //elseif (is_page('Our Process')) : ?>
+<!--    <script>-->
+<!--        $(document).ready(function () {-->
+<!--            setActiveTab('Our Process');-->
+<!--        });-->
+<!--    </script>-->
+<?php //elseif (is_page('Contact Us')) : ?>
+<!--    <script>-->
+<!--        $(document).ready(function () {-->
+<!--            setActiveTab('Contact Us');-->
+<!--        });-->
+<!--    </script>-->
+<?php //endif; ?>
  <header id="banner-header">
-    <img class="banner" src="<?php echo wp_get_attachment_url(391); ?>" alt="Kind Technology Services" />
+     <p>Kind Technology Services</p>
+<!--    <img class="banner" src="--><?php //echo wp_get_attachment_url(391); ?><!--" alt="Kind Technology Services" />-->
  </header>
+<div class="nav-wrapper">
+    <div class="nav-bar">
+
+        <div class="btn-navbar">
+            <a id="mobile-nav-button-minus" href="#" style="display: none;"><img
+                    src="<?php echo wp_get_attachment_url(337); ?>" alt="Mobile Navigation"/></a>
+            <a id="mobile-nav-button-plus" href="#"><img src="<?php echo wp_get_attachment_url(336); ?>"
+                                                         alt="Mobile Navigation"/></a>
+        </div>
+
+        <div class="nav">
+
+            <ul class="tabs">
+                <li>
+                    <a class="menu_link" id="home_link" href="/wp/home">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a class="menu_link" id="about_link" href="/wp/about-us">
+                        About Us
+                    </a>
+                </li>
+                <li>
+                    <a class="menu_link" id="services_link" href="/wp/services">
+                        Services
+                    </a>
+                </li>
+                <li>
+                    <a class="menu_link" id="processes_link" href="/wp/our-process">
+                        Our Process
+                    </a>
+                </li>
+                <li>
+                    <a class="menu_link" id="contact_link" href="/wp/contact-us">
+                        Contact Us
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div id="mobile-tab-nav">
+            <?php
+            $args = array(
+                'theme_location' => 'primary',
+                'depth' => 3,
+                'container' => false,
+                'menu_class' => 'mobile-nav',
+                'fallback_cb' => false,
+                'walker' => new heavenly_bootstrap_walker_nav_menu()
+            );
+            wp_nav_menu($args);
+            ?>
+        </div>
+    </div>
+</div>
 <?php
 if(is_front_page()) //get_template_part('homepage','top');
 ?>
